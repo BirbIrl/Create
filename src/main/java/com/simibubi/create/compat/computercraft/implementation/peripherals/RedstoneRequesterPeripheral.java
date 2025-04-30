3b1fee71480e3a70fe4260d5d88cd6e81322c967
ShimmerGlass <mathildegilles49@gmail.con>
2025-04-30 20:14:58 +0200

    RedstoneRequesterPeripheral: add configureCraft for mech crafter compat

--- src/main/java/com/simibubi/create/compat/computercraft/implementation/peripherals/RedstoneRequesterPeripheral.java
+++ src/main/java/com/simibubi/create/compat/computercraft/implementation/peripherals/RedstoneRequesterPeripheral.java
@@ -41,39 +41,24 @@ public class RedstoneRequesterPeripheral extends SyncedPeripheral<RedstoneReques
 
 	@LuaFunction(mainThread = true)
 	public final void configure(IArguments arguments) throws LuaException {
+		List<BigItemStack> orderStacks = generateOrder(arguments);
+		this.blockEntity.encodedRequest = PackageOrderWithCrafts.simple(orderStacks);
+		this.blockEntity.notifyUpdate();
+	}
 
-		ArrayList<BigItemStack> list = new ArrayList<>();
+	@LuaFunction(mainThread = true)
+	public final void configureCraft(IArguments arguments) throws LuaException {
+		int count = arguments.getInt(0);
+		arguments = arguments.drop(1);
 
-		for (int i = 0; i <= 8; i++) {
-			if (arguments.get(i) == null) {
-				list.add(new BigItemStack(ItemStack.EMPTY, 1));
-			} else {
-				Map<?, ?> itemData = arguments.getTable(i);
+		List<BigItemStack> orderStacks = generateOrder(arguments);
 
-				if (!(itemData instanceof Map)) {
-					throw new LuaException("Table or nil expected for each item entry");
-				}
-				String itemName = "minecraft:air";
-				if (itemData.get("name") instanceof String) {
-					itemName = (String) itemData.get("name");
-				}
-				int count = 1;
-				if (itemData.get("count") instanceof Number) {
-					Object countObj = itemData.get("count");
-					count = (countObj instanceof Number) ? ((Number) countObj).intValue() : 1;
-					if (count > 256)
-						throw new LuaException("Count for item " + itemName + " exceeds 256");
-				}
-				ResourceLocation resourceLocation = ResourceLocation.tryParse(itemName);
-				ItemLike item = BuiltInRegistries.ITEM.get(resourceLocation);
-				list.add(new BigItemStack(new ItemStack(item), count));
-			}
-		}
+		PackageOrder order = new PackageOrder(orderStacks);
+		CraftingEntry orderContext = new CraftingEntry(new PackageOrder(orderStacks.stream()
+				.map(stack -> new BigItemStack(stack.stack.copyWithCount(1)))
+				.toList()), count);
 
-		// XXX Maybe make the CraftingEntry's count configurable through the API?
-		this.blockEntity.encodedRequest = new PackageOrderWithCrafts(
-				new PackageOrder(list),
-				List.of(new CraftingEntry(new PackageOrder(list), 1)));
+		this.blockEntity.encodedRequest = new PackageOrderWithCrafts(order, List.of(orderContext));
 		this.blockEntity.notifyUpdate();
 	}
 
@@ -116,4 +101,35 @@ public class RedstoneRequesterPeripheral extends SyncedPeripheral<RedstoneReques
 		return "Create_RedstoneRequester";
 	}
 
+	private List<BigItemStack> generateOrder(IArguments arguments) throws LuaException {
+		ArrayList<BigItemStack> list = new ArrayList<>();
+
+		for (int i = 0; i <= 8; i++) {
+			if (arguments.get(i) == null) {
+				list.add(new BigItemStack(ItemStack.EMPTY, 1));
+			} else {
+				Map<?, ?> itemData = arguments.getTable(i);
+
+				if (!(itemData instanceof Map)) {
+					throw new LuaException("Table or nil expected for each item entry");
+				}
+				String itemName = "minecraft:air";
+				if (itemData.get("name") instanceof String) {
+					itemName = (String) itemData.get("name");
+				}
+				int count = 1;
+				if (itemData.get("count") instanceof Number) {
+					Object countObj = itemData.get("count");
+					count = (countObj instanceof Number) ? ((Number) countObj).intValue() : 1;
+					if (count > 256)
+						throw new LuaException("Count for item " + itemName + " exceeds 256");
+				}
+				ResourceLocation resourceLocation = ResourceLocation.tryParse(itemName);
+				ItemLike item = BuiltInRegistries.ITEM.get(resourceLocation);
+				list.add(new BigItemStack(new ItemStack(item), count));
+			}
+		}
+
+		return list;
+	}
 }
