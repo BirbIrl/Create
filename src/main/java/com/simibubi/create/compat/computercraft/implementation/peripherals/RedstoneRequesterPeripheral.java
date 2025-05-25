5a318ea479ef0bb85c5a4467720a901bfb1985d6
MaxOrtGit <70595776+MaxOrtGit@users.noreply.github.com>
2025-05-25 08:35:29 -0700

    configureCraft allows for just string

--- src/main/java/com/simibubi/create/compat/computercraft/implementation/peripherals/RedstoneRequesterPeripheral.java
+++ src/main/java/com/simibubi/create/compat/computercraft/implementation/peripherals/RedstoneRequesterPeripheral.java
@@ -95,29 +95,31 @@ public class RedstoneRequesterPeripheral extends SyncedPeripheral<RedstoneReques
 	private List<BigItemStack> generateOrder(IArguments arguments) throws LuaException {
 		ArrayList<BigItemStack> list = new ArrayList<>();
 
-		for (int i = 0; i <= 8; i++) {
+		for (int i = 0; i < 9; i++) {
 			if (arguments.get(i) == null) {
 				list.add(new BigItemStack(ItemStack.EMPTY, 1));
 			} else {
-				Map<?, ?> itemData = arguments.getTable(i);
-
-				if (!(itemData instanceof Map)) {
-					throw new LuaException("Table or nil expected for each item entry");
-				}
-				String itemName = "minecraft:air";
-				if (itemData.get("name") instanceof String) {
-					itemName = (String) itemData.get("name");
-				}
-				int count = 1;
-				if (itemData.get("count") instanceof Number) {
-					Object countObj = itemData.get("count");
-					count = (countObj instanceof Number) ? ((Number) countObj).intValue() : 1;
-					if (count > 256)
-						throw new LuaException("Count for item " + itemName + " exceeds 256");
-				}
-				ResourceLocation resourceLocation = ResourceLocation.tryParse(itemName);
-				ItemLike item = BuiltInRegistries.ITEM.get(resourceLocation);
-				list.add(new BigItemStack(new ItemStack(item), count));
+        Object arg = arguments.get(i);
+        if (arg instanceof String itemName) {
+          ResourceLocation resourceLocation = ResourceLocation.tryParse(itemName);
+          ItemLike item = BuiltInRegistries.ITEM.get(resourceLocation);
+          list.add(new BigItemStack(new ItemStack(item), 1));
+        } else if (arg instanceof Map<?, ?> itemData) {
+          String itemName = "minecraft:air";
+          if (itemData.get("name") instanceof String) {
+            itemName = (String) itemData.get("name");
+          }
+          int count = 1;
+          if (itemData.get("count") instanceof Number) {
+            Object countObj = itemData.get("count");
+            count = (countObj instanceof Number) ? ((Number) countObj).intValue() : 1;
+            if (count > 256)
+              throw new LuaException("Count for item " + itemName + " exceeds 256");
+          }
+          ResourceLocation resourceLocation = ResourceLocation.tryParse(itemName);
+          ItemLike item = BuiltInRegistries.ITEM.get(resourceLocation);
+          list.add(new BigItemStack(new ItemStack(item), count));
+        }
 			}
 		}
 
