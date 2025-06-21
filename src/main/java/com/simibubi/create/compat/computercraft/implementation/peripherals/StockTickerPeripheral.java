68d406e1772b5fe9ca26955ab3f87d9f5481dab1
BirbIrl <birbirl@proton.me>
2025-06-21 07:57:35 +0200

    refactored .requestFiltered and added _requestCount functionality

--- src/main/java/com/simibubi/create/compat/computercraft/implementation/peripherals/StockTickerPeripheral.java
+++ src/main/java/com/simibubi/create/compat/computercraft/implementation/peripherals/StockTickerPeripheral.java
@@ -43,70 +43,65 @@ public class StockTickerPeripheral extends SyncedPeripheral<StockTickerBlockEnti
 		return result;
 	}
 
-	/*
-	 * for every item in the network, this will compare that item to the CC args
-	 * filter, a table that looks something like this:
-	 * {
-	 * name = "minecraft:jungle_log",
-	 * tags = {
-	 * ["minecraft:logs"] = true
-	 * },
-	 * count = 5
-	 * },
-	 * and the second optional String arg which is the address:
-	 * "home_address"
-	 * (default value "")
-	 *
-	 * It then adds items that match the name if provided, nbt if provided, have all
-	 * of the tags if provided, has all the enchants if provided and
-	 * stops looking after adding items equal to count or finishing
-	 * going through the summary.
-	 * filter of {} requests all items from the network trollface.jpeg
-	 */
 	@LuaFunction(mainThread = true)
-	public final int requestFiltered(IArguments arguments) throws LuaException {
-		if (!(arguments.get(0) instanceof Map<?, ?> filterTable))
-			throw new LuaException("Filter must be a table");
+	public final int requestFiltered(Optional<String> address, IArguments filters) throws LuaException {
 
-		for (Object key : filterTable.keySet())
-			if (!(key instanceof String))
-				throw new LuaException("Filter keys must be strings");
-
-		@SuppressWarnings("unchecked")
-		Map<String, Object> filter = (Map<String, Object>) filterTable;
-
-		int itemsRequested = Integer.MAX_VALUE;
-		if (arguments.get(2) instanceof Number) {
-			itemsRequested = ((Number) arguments.get(2)).intValue();
-			if (itemsRequested < 1)
-				throw new LuaException("Count must be a positive number or nil for all");
-		}
-		int itemsSent = 0;
-
-		String address;
-		// Computercraft has forced my hand to make this dollar store filter algo
-		List<BigItemStack> validItems = new ArrayList<>();
-		for (BigItemStack entry : blockEntity.getAccurateSummary().getStacks()) {
-			int foundItems = ComputerUtil.bigItemStackToLuaTableFilter(entry, filter);
-			if (foundItems > 0) {
-				int toTake = Math.min(foundItems, itemsRequested);
-				itemsRequested -= toTake;
-				itemsSent += toTake;
-				entry.count = toTake;
-				validItems.add(entry);
-			}
-			if (itemsRequested <= 0)
-				break;
-		}
-		if (arguments.get(1) instanceof String)
-			address = arguments.getString(1);
+		String targetAddress;
+		if (address.isPresent())
+			targetAddress = address.get();
 		else
-			address = "";
+			targetAddress = "";
 
-		PackageOrder order = new PackageOrder(validItems);
-		blockEntity.broadcastPackageRequest(RequestType.RESTOCK, order, null, address);
+		List<PackageOrder> orders = new ArrayList<>();
 
-		return itemsSent;
+		int totalItemsSent = 0;
+
+		for (int i = 1; i < filters.count(); i++) {
+			if (!(filters.get(i) instanceof Map<?, ?> filterTable))
+				throw new LuaException("Filter must be a table");
+
+			for (Object key : filterTable.keySet())
+				if (!(key instanceof String))
+					throw new LuaException("Filter keys must be strings");
+
+			@SuppressWarnings("unchecked")
+			Map<String, Object> filter = (Map<String, Object>) filterTable;
+
+			int itemsRequested = Integer.MAX_VALUE;
+			if (filterTable.containsKey("_requestCount")) {
+				Object requestCount = filterTable.get("_requestCount");
+				filterTable.remove("_requestCount");
+				if (requestCount instanceof Number) {
+					itemsRequested = ((Number) requestCount).intValue();
+					if (itemsRequested < 1)
+						throw new LuaException("_requestCount must be a positive number or nil for no limit");
+				} else
+					throw new LuaException("_requestCount must be a positive number or nil for no limit");
+			}
+
+			List<BigItemStack> validItems = new ArrayList<>();
+			for (BigItemStack entry : blockEntity.getAccurateSummary().getStacks()) {
+				int foundItems = ComputerUtil.bigItemStackToLuaTableFilter(entry, filter);
+				if (foundItems > 0) {
+					int toTake = Math.min(foundItems, itemsRequested);
+					itemsRequested -= toTake;
+					totalItemsSent += toTake;
+					entry.count = toTake;
+					validItems.add(entry);
+				}
+				if (itemsRequested <= 0)
+					break;
+			}
+
+			PackageOrder order = new PackageOrder(validItems);
+
+			orders.add(new PackageOrder(validItems));
+		}
+
+		for (var order : orders)
+			blockEntity.broadcastPackageRequest(RequestType.RESTOCK, order, null, targetAddress);
+
+		return totalItemsSent;
 	}
 
 	@LuaFunction(mainThread = true)
