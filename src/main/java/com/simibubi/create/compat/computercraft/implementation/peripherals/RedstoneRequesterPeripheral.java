be71b18651481713526b3e87709273e8d151b1b9
BirbIrl <birbirl@proton.me>
2025-06-24 18:37:34 +0200

    added .setConfiguration and .getConfiguration to RR also had to refactor .configure and .configureCraft to .setRequest and .setCraftingRequest for this

--- src/main/java/com/simibubi/create/compat/computercraft/implementation/peripherals/RedstoneRequesterPeripheral.java
+++ src/main/java/com/simibubi/create/compat/computercraft/implementation/peripherals/RedstoneRequesterPeripheral.java
@@ -36,14 +36,14 @@ public class RedstoneRequesterPeripheral extends SyncedPeripheral<RedstoneReques
 	}
 
 	@LuaFunction(mainThread = true)
-	public final void configure(IArguments arguments) throws LuaException {
+	public final void setRequest(IArguments arguments) throws LuaException {
 		List<BigItemStack> orderStacks = generateOrder(arguments);
 		this.blockEntity.encodedRequest = PackageOrderWithCrafts.simple(orderStacks);
 		this.blockEntity.notifyUpdate();
 	}
 
 	@LuaFunction(mainThread = true)
-	public final void configureCraft(IArguments arguments) throws LuaException {
+	public final void setCraftingRequest(IArguments arguments) throws LuaException {
 		int count = arguments.getInt(0);
 		arguments = arguments.drop(1);
 
@@ -51,11 +51,52 @@ public class RedstoneRequesterPeripheral extends SyncedPeripheral<RedstoneReques
 
 		PackageOrder order = new PackageOrder(orderStacks);
 		CraftingEntry orderContext = new CraftingEntry(new PackageOrder(orderStacks.stream()
-			.map(stack -> new BigItemStack(stack.stack.copyWithCount(1)))
-			.toList()), count);
+				.map(stack -> new BigItemStack(stack.stack.copyWithCount(1)))
+				.toList()), count);
 
 		this.blockEntity.encodedRequest = new PackageOrderWithCrafts(order, List.of(orderContext));
 		this.blockEntity.notifyUpdate();
+	}
+
+	@LuaFunction(mainThread = true)
+	public final Map<Integer, Map<String, ?>> getRequest() throws LuaException {
+		List<BigItemStack> stacks = blockEntity.encodedRequest.stacks();
+		Map<Integer, Map<String, ?>> result = new HashMap<>();
+		// Loop through the packageOrder get each bigItem stack
+		for (int i = 0; i < stacks.size(); i++) {
+			ItemStack stack = stacks.get(i).stack;
+			Map<String, Object> details = new HashMap<>(
+					VanillaDetailRegistries.ITEM_STACK.getDetails(stack));
+			if (!details.get("name").equals("minecraft:air")) {
+				details.put("count", stacks.get(i).count);
+				result.put(i + 1, details); // +1 because lua
+			}
+		}
+		return result;
+	}
+
+	@LuaFunction(mainThread = true)
+	public final String getConfiguration() throws LuaException {
+		if (blockEntity.allowPartialRequests)
+			return "allow_partial";
+		else
+			return "strict";
+	}
+
+	@LuaFunction(mainThread = true)
+	public final boolean setConfiguration(String config) throws LuaException {
+		if (config.equals("allow_partial")) {
+			blockEntity.allowPartialRequests = true;
+			blockEntity.notifyUpdate();
+			return true;
+		}
+		if (config.equals("strict")) {
+			blockEntity.allowPartialRequests = false;
+			blockEntity.notifyUpdate();
+			return true;
+		}
+		throw new LuaException("Unknown configuration: \"" + config
+				+ "\" Possible configurations are: \"allow_partial\" and \"strict\".");
 	}
 
 	@LuaFunction(mainThread = true)
@@ -67,23 +108,6 @@ public class RedstoneRequesterPeripheral extends SyncedPeripheral<RedstoneReques
 	@LuaFunction(mainThread = true)
 	public final String getAddress() throws LuaException {
 		return blockEntity.encodedTargetAdress;
-	}
-
-	@LuaFunction(mainThread = true)
-	public final Map<Integer, Map<String, ?>> getConfiguration() throws LuaException {
-		List<BigItemStack> stacks = blockEntity.encodedRequest.stacks();
-		Map<Integer, Map<String, ?>> result = new HashMap<>();
-		// Loop through the packageOrder get each bigItem stack
-		for (int i = 0; i < stacks.size(); i++) {
-			ItemStack stack = stacks.get(i).stack;
-			Map<String, Object> details = new HashMap<>(
-				VanillaDetailRegistries.ITEM_STACK.getDetails(stack));
-			if (!details.get("name").equals("minecraft:air")) {
-				details.put("count", stacks.get(i).count);
-				result.put(i + 1, details); // +1 because lua
-			}
-		}
-		return result;
 	}
 
 	@NotNull
@@ -99,27 +123,27 @@ public class RedstoneRequesterPeripheral extends SyncedPeripheral<RedstoneReques
 			if (arguments.get(i) == null) {
 				list.add(new BigItemStack(ItemStack.EMPTY, 1));
 			} else {
-        Object arg = arguments.get(i);
-        if (arg instanceof String itemName) {
-          ResourceLocation resourceLocation = ResourceLocation.tryParse(itemName);
-          ItemLike item = BuiltInRegistries.ITEM.get(resourceLocation);
-          list.add(new BigItemStack(new ItemStack(item), 1));
-        } else if (arg instanceof Map<?, ?> itemData) {
-          String itemName = "minecraft:air";
-          if (itemData.get("name") instanceof String) {
-            itemName = (String) itemData.get("name");
-          }
-          int count = 1;
-          if (itemData.get("count") instanceof Number) {
-            Object countObj = itemData.get("count");
-            count = (countObj instanceof Number) ? ((Number) countObj).intValue() : 1;
-            if (count > 256)
-              throw new LuaException("Count for item " + itemName + " exceeds 256");
-          }
-          ResourceLocation resourceLocation = ResourceLocation.tryParse(itemName);
-          ItemLike item = BuiltInRegistries.ITEM.get(resourceLocation);
-          list.add(new BigItemStack(new ItemStack(item), count));
-        }
+				Object arg = arguments.get(i);
+				if (arg instanceof String itemName) {
+					ResourceLocation resourceLocation = ResourceLocation.tryParse(itemName);
+					ItemLike item = BuiltInRegistries.ITEM.get(resourceLocation);
+					list.add(new BigItemStack(new ItemStack(item), 1));
+				} else if (arg instanceof Map<?, ?> itemData) {
+					String itemName = "minecraft:air";
+					if (itemData.get("name") instanceof String) {
+						itemName = (String) itemData.get("name");
+					}
+					int count = 1;
+					if (itemData.get("count") instanceof Number) {
+						Object countObj = itemData.get("count");
+						count = (countObj instanceof Number) ? ((Number) countObj).intValue() : 1;
+						if (count > 256)
+							throw new LuaException("Count for item " + itemName + " exceeds 256");
+					}
+					ResourceLocation resourceLocation = ResourceLocation.tryParse(itemName);
+					ItemLike item = BuiltInRegistries.ITEM.get(resourceLocation);
+					list.add(new BigItemStack(new ItemStack(item), count));
+				}
 			}
 		}
 
